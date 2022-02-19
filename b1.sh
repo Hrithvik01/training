951951d153aca83dc59b78f6bcaf7a9946f32f72
Hrithvik Atluri <hrithvikatluri@gmail.com>
2022-02-19 21:04:57 +0530

    b1 edit commit in master

--- b1.sh
+++ b1.sh
@@ -8,26 +8,10 @@ add_user(){
 
 }
 
-add_group(){
-    echo 'input new group name here'
-    read x
-    sudo groupadd $x
-    echo 'adding for'
-    $z
-    sudo usermod -a -G $x $USER
-    echo "This group is successfully added to $USER"
-    groups $USER
-    
-}
+
 addsomeone(){
-echo 'input here'
-read x
-if [ $x == '1' ]
-then
 add_user
-else [ $x == 2 ]
-add_group
-fi
+
 }
 
 
